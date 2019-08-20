fb63063ea0328ba5bdad3a26e0d09d7cefd2d5ff
marcotgs <marcot.gualberto@gmail.com>
2019-08-20 02:00:20 -0300

    Fix typescript files

---
+++ models/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable */
+
+export { default as acesso_empresasModelStatic } from "./acesso_empresas";
---
+++ test.js
@@ -0,0 +1,20 @@
+var AutoSequilize = require('./index.js');
+
+const auto = new AutoSequilize('AppWorkDB', 'database', "PB<W5%]P9HQKRtAk", {
+    host: 'localhost',
+    dialect: "mssql",
+    directory: "./models",
+    additional: {
+        timestamps: false
+    },
+    typescript: true,
+    tables: ["acesso_empresas"]
+});
+
+auto.run((err) => {
+    if (err) throw err;
+    else {
+        console.log("\x1b[32m", "Sucesso -> Arquivos gerados");
+        console.log("\x1b[0m");
+    }
+});
